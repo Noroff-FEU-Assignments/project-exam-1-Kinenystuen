25383600b4204b3b4017b807fb31b6331e9ab4c7
Kinenystuen <kine_on@hotmail.com>
2024-02-07 20:41:36 +0100

    vh testing

--- js/index.js
+++ js/index.js
@@ -9,3 +9,9 @@ import { openModal } from "./render/renderModal.js";
 import { validateForm } from "./contact.js";
 
 
+
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
